0f91ba23bdaf7e06e53ece3997a7f51ce0adc5f0
Dana <daniela.prajea@gmail.com>
2018-12-18 15:31:34 -0500

    "Moved files around"

--- frontend/src/NewQuestion/NewQuestion.js
+++ frontend/src/NewQuestion/NewQuestion.js
@@ -31,7 +31,7 @@ class NewQuestion extends Component {
             disabled:true
         })
         
-        await axios.post('http://localhost:8081', {
+        await axios.post('/api/newquestion', { // https://tourmapper-backend.herokuapp.com/
             title: this.state.title, 
             description: this.state.description,
         }, {
