bbec55ae3b58bec5376f0cbb22528c53ad62e160
Dana <daniela.prajea@gmail.com>
2019-01-14 17:58:50 -0500

    Separated typedefs into different files

--- server/Models/Typedefs.js
+++ server/Models/Typedefs.js
@@ -1,31 +1,8 @@
-const { gql } = require("apollo-server");
+const { fileLoader, mergeTypes } = require("merge-graphql-schemas");
+const path = require("path");
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # The Question typedef and its query
-  type Answer {
-    answer: String
-    author: String
-  }
+const typesArray = fileLoader(path.join(__dirname, "./typedefs"));
 
-  type Question {
-    id: ID!
-    title: String!
-    description: String!
-    answers: [Answer]
-  }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    questions: [Question]
-    question(id: ID!): Question
-  }
-  type Mutation {
-    createQuestion(title: String!, description: String!): Question
-    addAnswerToQuestion(id: ID!, answer: String!): Question
-  }
-`;
+const typeDefs = mergeTypes(typesArray, { all: true });
 
 module.exports = typeDefs;
