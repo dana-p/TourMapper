313aa54a4dd681c4d98bb2a5fc78ff94063464d5
Dana <daniela.prajea@gmail.com>
2019-01-30 17:36:47 -0500

    Auto formatting

--- frontend/src/SecuredRoute/SecuredRoute.js
+++ frontend/src/SecuredRoute/SecuredRoute.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Route } from 'react-router-dom'
-import auth0Client from '../Auth'
+import React from "react";
+import { Route } from "react-router-dom";
+import auth0Client from "../Auth";
 
-function SecuredRoute(props){
-    const {component: Component, path, checkingSession} = props; 
-    return (
-        <Route path={path} render={()=>{
-            // Check if token is saved 
-            if (checkingSession) return <h3 className="text-center">Validating session...</h3>
+function SecuredRoute(props) {
+  const { component: Component, path, checkingSession } = props;
+  return (
+    <Route
+      path={path}
+      render={() => {
+        // Check if token is saved
+        if (checkingSession)
+          return <h3 className="text-center">Validating session...</h3>;
 
-            // Check if user is authenticated before showing page
-            if (!auth0Client.isAuthenticated()){
-                auth0Client.signIn(); 
-                return <div></div>
-            }
-            return <Component/>
-            }} />
-    );
+        // Check if user is authenticated before showing page
+        if (!auth0Client.isAuthenticated()) {
+          auth0Client.signIn();
+          return <div />;
+        }
+        return <Component />;
+      }}
+    />
+  );
 }
 
-export default SecuredRoute; 
+export default SecuredRoute;
