d524dcfc57235ab5731198415a1602c28270f645
Dana <daniela.prajea@gmail.com>
2019-02-19 10:09:45 -0500

    Moved require, maybe that's why?

--- index.js
+++ index.js
@@ -99,6 +99,14 @@ monDb.on("error", function() {
 
 /*
  |--------------------------------------
+ | Google Requests
+ |--------------------------------------
+*/
+
+require("./server/api")(app);
+
+/*
+ |--------------------------------------
  | Routes
  |--------------------------------------
  */
@@ -127,14 +135,6 @@ if (process.env.NODE_ENV !== "dev") {
 // server.express.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 // });
-
-/*
- |--------------------------------------
- | Google Requests
- |--------------------------------------
-*/
-
-require("./server/api")(app);
 
 /*
  |--------------------------------------
